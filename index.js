17562338cca08cb46e831d37e6cfd463cd028188
SyedaAramish <161924721+SyedaAramish@users.noreply.github.com>
2024-04-07 10:50:47 -0700

    add more currencies

--- index.js
+++ index.js
@@ -1,17 +1,40 @@
+#! /usr/bin/env node
 import inquirer from "inquirer";
 const currency = {
     PKR: 1,
     USD: 0.0036,
     EUR: 0.0033,
-    INR: 0.30,
+    INR: 0.3,
     GBP: 0.0028,
+    CAD: 0.00489,
+    JPY: 0.5457,
+    CHF: 0.0032,
+    SGD: 0.00485,
+    LKR: 1.076,
+    SAR: 0.013,
+    TRY: 0.1153,
+    QAR: 0.013, //QATARI RIYAL
 };
 let ans = await inquirer.prompt([
     {
         name: "from",
         message: "enter from Currency! ",
         type: "list",
-        choices: ["PKR", "USD", "EUR", "INR", "GBP"],
+        choices: [
+            "PKR",
+            "USD",
+            "EUR",
+            "INR",
+            "GBP",
+            "CAD",
+            "JPY",
+            "CHF",
+            "SGD",
+            "LKR",
+            "SAR",
+            "TRY",
+            "QAR",
+        ],
     },
     {
         name: "to",
@@ -23,7 +46,7 @@ let ans = await inquirer.prompt([
         name: "amount",
         message: "Enter your amount",
         type: "number",
-    }
+    },
 ]);
 let from_amount = currency[ans.from];
 let to_amount = currency[ans.to];
--- index.ts
+++ index.ts
@@ -1,45 +1,59 @@
-import inquirer from "inquirer"
-
+#! /usr/bin/env node
+import inquirer from "inquirer";
 
 const currency: any = {
-PKR:1,      //base currency
-USD:0.0036,
-EUR:0.0033,
-INR:0.30,
-GBP:0.0028,
+  PKR: 1, //base currency
+  USD: 0.0036, // US DOLLAR
+  EUR: 0.0033, // EURO
+  INR: 0.3, // INDIAN RUPEE
+  GBP: 0.0028, //BRITISH POUND
+  CAD: 0.00489, // CANADIAN DOLLAR
+  JPY: 0.5457, // JAPANESE YEN
+  CHF: 0.0032, //SWISS FRANC
+  SGD: 0.00485, //SINGAPORE DOLLARS
+  LKR: 1.076, //SRI LANKAN RUPEES
+  SAR: 0.013, //SAUDI ARABIAN RIYALS
+  TRY: 0.1153, //TURKISH LIRE
+  QAR: 0.013, //QATARI RIYAL
 };
 
-
-let ans = await inquirer.prompt(
-    [
-        {
+let ans = await inquirer.prompt([
+  {
     name: "from",
     message: "enter from Currency! ",
     type: "list",
-    choices: ["PKR","USD", "EUR", "INR", "GBP"],
-        },
-        {
-            name: "to",
-            message: "enter to Currency! ",
-            type: "list",
-            choices: ["PKR","USD", "EUR", "INR", "GBP"],
-                },
-                {
-                    name:"amount",
-                    message:"Enter your amount",
-                    type:"number",
-                }
-    ]
-)
+    choices: [
+      "PKR",
+      "USD",
+      "EUR",
+      "INR",
+      "GBP",
+      "CAD",
+      "JPY",
+      "CHF",
+      "SGD",
+      "LKR",
+      "SAR",
+      "TRY",
+      "QAR",
+    ],
+  },
+  {
+    name: "to",
+    message: "enter to Currency! ",
+    type: "list",
+    choices: ["PKR", "USD", "EUR", "INR", "GBP"],
+  },
+  {
+    name: "amount",
+    message: "Enter your amount",
+    type: "number",
+  },
+]);
+let from_amount = currency[ans.from];
+let to_amount = currency[ans.to];
+let amount = ans.amount;
+let baseAmount = amount / from_amount;
 
-let from_amount = currency[ans.from]
-let to_amount = currency[ans.to]
-let amount = ans.amount
-let baseAmount = amount / from_amount
-let newAmount = baseAmount*to_amount
-
-
+let newAmount = baseAmount * to_amount;
 console.log(newAmount);
-
-
-
